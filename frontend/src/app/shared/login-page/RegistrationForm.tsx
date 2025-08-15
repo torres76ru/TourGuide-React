f5d4fe83b21e997c4fa4637f7e17cfde7d6a061b
torres76ru <torres76ru@gmail.com>
2025-08-15 18:50:33 +0300

    fix

--- frontend/src/app/shared/login-page/RegistrationForm.tsx
+++ frontend/src/app/shared/login-page/RegistrationForm.tsx
@@ -1,53 +1,97 @@
 import type { MouseEventHandler } from "react";
 import Button from "./Button";
-import Input from "./Input";
+import Input from "../../../shared/ui/Input/Input";
 import PasswordInput from "./PasswordInput";
 
 interface EntryFormProps {
-    className : string;
-    formData: {
-        firstName: string;
-        lastName: string;
-        email: string;
-        password: string;
-    };
-    errors: {
-        firstName: string;
-        lastName: string;
-        email: string;
-        password: string;
-    };
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    onClick?: MouseEventHandler<HTMLButtonElement>;
+  className: string;
+  formData: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+  };
+  errors: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+  };
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function RegistrationForm({className, formData, errors, onChange, onClick} : EntryFormProps) {
-    return (
-        <>
-        <form action="" className={`registration__registration-form ${className}`}>
-            <Input label="Имя" id="reg__first-name-id" type="text" 
-            value={formData.firstName}
-            onChange={onChange}
-            error={errors.firstName}></Input>
-            <Input label="Фамилия" id="reg__last-name-id" type="text"
-            value={formData.lastName}
-            onChange={onChange}
-            error={errors.lastName}></Input>
-            <Input label="Логин" id="reg__email-id" type="email" placeholder="Почта"
-            value={formData.email}
-            onChange={onChange}
-            error={errors.email}></Input>
-            <PasswordInput id="reg__password-id" className="password-container--margin"></PasswordInput>
-            <Input label="Повторите пароль" id="reg__repeat-password-id" type="text"
-            value={formData.password}
-            onChange={onChange}
-            error={errors.password}></Input>
-            <div className="registration__checkbox-container">
-                <input className="registration__checkbox" type="checkbox" id="reg__checkbox-id"/>
-                <label className="registration__checkbox-lable" htmlFor="reg__checkbox-id">Я прочитал и согласен с политикой данного сервиса.</label>
-            </div>
-            <Button className="registration__button--size" onClick={onClick} type="submit">Зарегистрироваться</Button>
-        </form>
-        </>
-    )
+export default function RegistrationForm({
+  className,
+  formData,
+  errors,
+  onChange,
+  onClick,
+}: EntryFormProps) {
+  return (
+    <>
+      <form
+        action=""
+        className={`registration__registration-form ${className}`}
+      >
+        <Input
+          label="Имя"
+          id="reg__first-name-id"
+          type="text"
+          value={formData.firstName}
+          onChange={onChange}
+          error={errors.firstName}
+        ></Input>
+        <Input
+          label="Фамилия"
+          id="reg__last-name-id"
+          type="text"
+          value={formData.lastName}
+          onChange={onChange}
+          error={errors.lastName}
+        ></Input>
+        <Input
+          label="Логин"
+          id="reg__email-id"
+          type="email"
+          placeholder="Почта"
+          value={formData.email}
+          onChange={onChange}
+          error={errors.email}
+        ></Input>
+        <PasswordInput
+          id="reg__password-id"
+          className="password-container--margin"
+        ></PasswordInput>
+        <Input
+          label="Повторите пароль"
+          id="reg__repeat-password-id"
+          type="text"
+          value={formData.password}
+          onChange={onChange}
+          error={errors.password}
+        ></Input>
+        <div className="registration__checkbox-container">
+          <input
+            className="registration__checkbox"
+            type="checkbox"
+            id="reg__checkbox-id"
+          />
+          <label
+            className="registration__checkbox-lable"
+            htmlFor="reg__checkbox-id"
+          >
+            Я прочитал и согласен с политикой данного сервиса.
+          </label>
+        </div>
+        <Button
+          className="registration__button--size"
+          onClick={onClick}
+          type="submit"
+        >
+          Зарегистрироваться
+        </Button>
+      </form>
+    </>
+  );
 }
--- frontend/src/pages/login-page/index.tsx
+++ frontend/src/pages/login-page/index.tsx
@@ -1,20 +1,15 @@
-import { useNavigate } from "react-router";
 import { useState } from "react";
 import Button from "../../app/shared/login-page/Button";
 import RegistrationChoice from "../../app/shared/login-page/RegistrationChoice";
 import EntryForm from "../../app/shared/login-page/EntryForm";
 import RegistrationForm from "../../app/shared/login-page/RegistrationForm";
-import "./style.css"
+import "./style.css";
 
 const LoginPage = () => {
-  const navigate = useNavigate();
-
-  const handleRedirect = () => {
-    navigate("/");
-  };
-
   const [isVisible, setIsVisible] = useState(false);
-  const [activeBlock, setActiveBlock] = useState<null| 'entry' | 'choice' |'registration'>(null);
+  const [activeBlock, setActiveBlock] = useState<
+    null | "entry" | "choice" | "registration"
+  >(null);
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -30,42 +25,43 @@ const LoginPage = () => {
   });
 
   function CheckIsVisible(className?: string): string {
-  return isVisible ? className || '' : '';
-}
+    return isVisible ? className || "" : "";
+  }
 
   function GoBack() {
-    if(activeBlock === null) setIsVisible(false) 
-    else if (activeBlock === 'entry' || activeBlock === 'choice') setActiveBlock(null)
-    else if (activeBlock === 'registration') setActiveBlock('choice')
+    if (activeBlock === null) setIsVisible(false);
+    else if (activeBlock === "entry" || activeBlock === "choice")
+      setActiveBlock(null);
+    else if (activeBlock === "registration") setActiveBlock("choice");
     setFormData({ firstName: "", lastName: "", email: "", password: "" });
-    setErrors({ firstName: "", lastName: "",email: "", password: "" });
+    setErrors({ firstName: "", lastName: "", email: "", password: "" });
   }
 
   const normalizeId = (id: string) => {
-    if (id.includes('repeat-password')) {
-    return 'password';
-  }
-  return id
-    .replace(/^(entry__|reg__)/, '')
-    .replace(/-id$/, '')           
-    .replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
-};
+    if (id.includes("repeat-password")) {
+      return "password";
+    }
+    return id
+      .replace(/^(entry__|reg__)/, "")
+      .replace(/-id$/, "")
+      .replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
+  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
     const fieldName = normalizeId(id);
-    
+
     setFormData({ ...formData, [fieldName]: value });
     setErrors({ ...errors, [fieldName]: "" });
-};
+  };
 
   // Валидация
   const validate = () => {
     let valid = true;
     const newErrors = { ...errors };
 
-    if (activeBlock === 'registration') {
-        // Валидация имени
+    if (activeBlock === "registration") {
+      // Валидация имени
       if (!formData.firstName.trim()) {
         newErrors.firstName = "Имя обязательно";
         valid = false;
@@ -105,24 +101,70 @@ const LoginPage = () => {
 
   return (
     <div>
-      <button className={`btn ${CheckIsVisible('btn--click')}`} onClick={() => setIsVisible(true)}>Войти</button>
+      <button
+        className={`btn ${CheckIsVisible("btn--click")}`}
+        onClick={() => setIsVisible(true)}
+      >
+        Войти
+      </button>
       <div className="registration">
-          <button className={`registration__btn-back ${CheckIsVisible('registration__btn-back--visible')}`}
-          onClick={GoBack}><img src="/src/assets/icons/Arrow.svg"/></button>
-          <div className={`registration__img ${CheckIsVisible('registration__img--visible')}`}>
-              <div className={`registration__blackout ${CheckIsVisible('registration__blackout--visible')}`}></div>
+        <button
+          className={`registration__btn-back ${CheckIsVisible(
+            "registration__btn-back--visible"
+          )}`}
+          onClick={GoBack}
+        >
+          <img src="/src/assets/icons/Arrow.svg" />
+        </button>
+        <div
+          className={`registration__img ${CheckIsVisible(
+            "registration__img--visible"
+          )}`}
+        >
+          <div
+            className={`registration__blackout ${CheckIsVisible(
+              "registration__blackout--visible"
+            )}`}
+          ></div>
+        </div>
+        <div
+          className={`registration__area container ${CheckIsVisible(
+            "registration__area--visible"
+          )}`}
+        >
+          <h3 className="registration__title">TourGuide</h3>
+          <p className="registration__slogan">
+            Отправляйся в путешествие с нами
+          </p>
+          <div
+            className={`registration__buttons ${
+              activeBlock === null ? "active-block" : ""
+            }`}
+          >
+            <Button onClick={() => setActiveBlock("entry")}>Войти</Button>
+            <Button onClick={() => setActiveBlock("choice")}>
+              Регистрация
+            </Button>
           </div>
-          <div className={`registration__area container ${CheckIsVisible('registration__area--visible')}`}>
-              <h3 className="registration__title">TourGuide</h3>
-              <p className="registration__slogan">Отправляйся в путешествие с нами</p>
-              <div className={`registration__buttons ${activeBlock === null ? 'active-block' : ''}`}>
-                  <Button onClick={() => setActiveBlock('entry')}>Войти</Button>
-                  <Button onClick={() => setActiveBlock('choice')}>Регистрация</Button>
-              </div>
-              <EntryForm className={activeBlock === 'entry' ? 'active-block' : ''} value={formData.email} onChange={handleChange} error={errors.email} onClick={handleSubmit}/>
-              <RegistrationChoice className={activeBlock === 'choice' ? 'active-block' : ''} onClick={() => setActiveBlock('registration')}/>
-              <RegistrationForm className={activeBlock === 'registration' ? 'active-block' : ''} formData={formData} onChange={handleChange} errors={errors} onClick={handleSubmit}/>
-          </div>
+          <EntryForm
+            className={activeBlock === "entry" ? "active-block" : ""}
+            value={formData.email}
+            onChange={handleChange}
+            error={errors.email}
+            onClick={handleSubmit}
+          />
+          <RegistrationChoice
+            className={activeBlock === "choice" ? "active-block" : ""}
+            onClick={() => setActiveBlock("registration")}
+          />
+          <RegistrationForm
+            className={activeBlock === "registration" ? "active-block" : ""}
+            formData={formData}
+            onChange={handleChange}
+            errors={errors}
+            onClick={handleSubmit}
+          />
+        </div>
       </div>
       {/* <button onClick={handleRedirect}>Go to Main</button> */}
     </div>
