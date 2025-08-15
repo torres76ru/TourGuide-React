a8cb5edfb396e0e58f94a5d8b440c15c417dc0b3
torres76ru <torres76ru@gmail.com>
2025-08-15 16:45:41 +0300

    add exports in widgets

--- frontend/src/widgets/index.tsx
+++ frontend/src/widgets/index.tsx
@@ -2,3 +2,6 @@ export { default as Header } from "./Header";
 export { default as WhereToGo } from "./where-to-go";
 export { default as Carousel } from "./Carousel/ui";
 export { default as SightCard } from "./SightCard/ui";
+export { default as Modal } from "./Modal/ui";
+export { default as Burger } from "./Burger/ui";
+export { default as Footer } from "./Footer/";
