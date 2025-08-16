00f9fabb29efd7e612fbf40cb0de180e46e2a0d6
torres76ru <torres76ru@gmail.com>
2025-08-16 10:47:09 +0300

    fix

--- frontend/src/app/routing/routes.ts
+++ frontend/src/app/routing/routes.ts
@@ -1,7 +1,8 @@
-import { LoginPage, MainPage, Test } from "../../pages";
+import { LoginPage, MainPage, SightPage, Test } from "pages";
 
 export const routes = [
   { path: "/", element: MainPage },
   { path: "/auth", element: LoginPage },
   { path: "/test", element: Test },
+  { path: "/sight", element: SightPage },
 ];
---
+++ frontend/src/pages/sight-page/index.tsx
@@ -0,0 +1,5 @@
+const SightPage = () => {
+  return <div>Достопримечательность</div>;
+};
+
+export default SightPage;
