a4b91a9084fd7801ae22e25dc6ac8d6bad161ab4
ddcringe <danya.kornev.999@gmail.com>
2025-08-23 19:24:39 +0300

    fixed settings

--- backend/django_server/settings.py
+++ backend/django_server/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS = True
+CORS_ALLOWED_ORIGINS =  ["http://176.123.161.85]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
