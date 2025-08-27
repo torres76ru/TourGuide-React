4271c73af0e0f693b53c220051743158e751ae7e
Егор Манапов <manapov.egor01@mail.ru>
2025-08-27 12:48:36 +0300

    fix leaderbord main_photo -> main_photo_url

--- backend/leaderboard/serializers.py
+++ backend/leaderboard/serializers.py
@@ -5,7 +5,13 @@ class LeaderboardSerializer(serializers.ModelSerializer):
     weighted_average = serializers.FloatField(read_only=True)
     average_rating = serializers.FloatField(read_only=True)
     rating_count = serializers.IntegerField(read_only=True)
+    main_photo_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Attraction
-        fields = ['id', 'name', 'category', 'address', 'weighted_average', 'average_rating', 'rating_count','description_short','main_photo']
+        fields = ['id', 'name', 'category', 'address', 'weighted_average', 'average_rating', 'rating_count','description_short','main_photo_url']
+
+    def get_main_photo_url(self, obj):
+        if obj.main_photo:
+            return obj.main_photo.url
+        return None
